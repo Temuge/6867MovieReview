503e03bdff56abfcee64b3ec90b81348ca29b906
Patricia Suriana <patricia.suriana@gmail.com>
2013-11-17 14:56:35 -0500

    update translate.m to handle either integer or cell array

--- translate.m
+++ translate.m
@@ -1,11 +1,26 @@
 % Given an array of integer, return the string representation
 
-function [string] = translate(int_array, int2word_table)
-    cell_array = num2cell(int_array);
+function [string] = translate(array, int2word_table)
     string = '';
+    
+    is_integer = false;
+    cell_array = array;
+    if isnumeric(array)
+       is_integer = true; 
+       cell_array = num2cell(array);
+    else
+        if iscell(array)
+            fprintf('ERROR: array has to be either an integer array or cell array');
+            return; 
+        end
+    end
+    
     for i = 1:length(cell_array)
         cell = cell_array(i);
-        key = num2str(cell{1});
+        key = cell{1};
+        if is_integer
+            key = num2str(key);
+        end
         word = int2word_table(key);
         string = sprintf('%s %s', string, word);
         if mod(i, 20) == 0
